e39dffa29923d3944a4bb00c4075507e837ef673
Samuel Moreira <genesis.samuel@gmail.com>
2011-11-05 18:41:00 +0000

    ScreenCast 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'a1e5b32e69c0dc1658180b8770db61b3f30c41c161af7f5819b68dc2a6c60e68274e632cfbf72ca3bbdc4ff016316e5725bc64984e41f9687d3160cedc1125e8'
